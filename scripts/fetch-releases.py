03ea66ed787e7bf6d5bd6678a968ccc114ce2f4e
Abhijat Malviya <malviya.abhijat@gmail.comm>
2025-10-08 13:37:21 +0530

    Add APT repo
    Signed-off-by: Abhijat Malviya <malviya.abhijat@gmail.comm>

--- scripts/fetch-releases.py
+++ scripts/fetch-releases.py
@@ -19,20 +19,25 @@ class Package:
     download_url: str
     version: str
     filename: str
+    arch: str
 
     @staticmethod
     def from_url(url: str) -> "Package":
         tokens = url.split('/')
         filename = tokens[-1]
         kind = AssetKind.RPM if filename.endswith(".rpm") else AssetKind.DEB
-        return Package(kind=kind, download_url=url, version=tokens[-2], filename=filename)
+        if kind == AssetKind.DEB:
+            arch = filename.split(".")[0].split("_")[1]
+        else:
+            arch = filename.split(".")[1]
+        return Package(kind=kind, download_url=url, version=tokens[-2], filename=filename, arch=arch)
 
     def storage_path(self, root: str) -> str:
         match self.kind:
             case AssetKind.RPM:
                 return os.path.join(root, "rpm", self.version)
             case AssetKind.DEB:
-                return os.path.join(root, "deb", self.version)
+                return os.path.join("deb_tmp", self.arch, self.version)
 
 
 def collect_download_urls() -> list[Package]:
@@ -48,7 +53,18 @@ def collect_download_urls() -> list[Package]:
 
 
 def download_packages(root: str, packages: list[Package]):
+    # The debian repository building tool, reprepo, only supports a single package per version by default.
+    # The ability to support multiple versions has been added but is not present in ubuntu-latest on
+    # github action runners yet. So we only download one package, the latest, for ubuntu.
+    # The rest of the scripts work on a set of packages, so that when the Limit parameter is supported,
+    # we can remove this flag and start hosting more than the latest versions.
+    # Another alternative would be to use the components feature of reprepo, but it would involve updating
+    # the repository definition itself for each release, which is a bad experience for end users.
+    deb_done = False
     for package in packages:
+        if package.kind == AssetKind.DEB and deb_done:
+            continue
+
         print(f"Downloading {package.download_url}")
         path = package.storage_path(root)
         if not os.path.exists(path):
@@ -61,6 +77,8 @@ def download_packages(root: str, packages: list[Package]):
             f.write(response.content)
         print(f"Downloaded {package.download_url}")
         time.sleep(0.5)
+        if package.kind == AssetKind.DEB:
+            deb_done = True
 
 
 def main():
